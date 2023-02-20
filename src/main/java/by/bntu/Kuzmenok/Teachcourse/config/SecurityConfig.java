2b406338048347f81ca6198b91a3a2c07036c36f
sBruttos <leonid2168@gmail.com>
2023-02-20 14:23:10 +0300

    add thymeleaf in html

---
+++ src/main/java/by/bntu/Kuzmenok/Teachcourse/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package by.bntu.Kuzmenok.Teachcourse.config;
+
+import by.bntu.Kuzmenok.Teachcourse.security.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login, /registration").permitAll()
+                .antMatchers("/profile").authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("j_username")
+                .passwordParameter("j_password")
+                .defaultSuccessUrl("/main")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/resources/**", "/static/**");
+    }
+}
+
---
+++ src/main/java/by/bntu/Kuzmenok/Teachcourse/controller/RegistrationController.java
@@ -0,0 +1,47 @@
+package by.bntu.Kuzmenok.Teachcourse.controller;
+
+import by.bntu.Kuzmenok.Teachcourse.entity.User;
+import by.bntu.Kuzmenok.Teachcourse.security.UserDetailsServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class RegistrationController {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public RegistrationController(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @GetMapping("/registration")
+    public String getRegistrationPage(Model model) {
+        model.addAttribute("userForm", new User());
+        return "/registration";
+    }
+
+    @PostMapping("/registration")
+    public String makeRegistration(@ModelAttribute("userForm") @Valid User user,
+                                   BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "/registration";
+        }
+
+        if (!userDetailsService.saveNewUser(user)) {
+            bindingResult.addError(new FieldError("userForm", "email",
+                    "User with this login is already exists"));
+            return "/registration";
+        }
+
+        return "redirect:/login";
+    }
+}
